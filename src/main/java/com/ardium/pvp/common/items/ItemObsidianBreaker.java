c1204d914041f18c0fbd7ba0aae3e58db16a80f8
Snapcoze <snapcoze@gmail.com>
2022-05-21 20:25:53 +0200

    Update ItemObsidianBreaker.java

--- src/main/java/com/ardium/pvp/common/items/ItemObsidianBreaker.java
+++ src/main/java/com/ardium/pvp/common/items/ItemObsidianBreaker.java
@@ -12,41 +12,34 @@ import net.minecraft.world.World;
 public class ItemObsidianBreaker extends Item {
 
     public ItemObsidianBreaker() {
-
-
+        this.setMaxStackSize(1);
+        this.setMaxDamage(1);
     }
 
     @Override
-    public ItemStack onItemRightClick(ItemStack item, World world, EntityPlayer player) {
+    public ItemStack onItemRightClick(final ItemStack par1ItemStack, final World par2World, final EntityPlayer par3EntityPlayer) {
 
-        if (world.isRemote) {
-            final MovingObjectPosition movingobjectposition = this.getMovingObjectPositionFromPlayer(world, player, true);
-            if (movingobjectposition == null) {
-                return super.onItemRightClick(item, world, player);
-            }
-            if (movingobjectposition.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
-                final int i = movingobjectposition.blockX;
-                final int j = movingobjectposition.blockY;
-                final int k = movingobjectposition.blockZ;
-                if (!world.canMineBlock(player, i, j, k)) {
-                    return super.onItemRightClick(item, world, player);
-                }
-                if (!player.canPlayerEdit(i, j, k, movingobjectposition.sideHit, item)) {
-                    return super.onItemRightClick(item, world, player);
-                }
-                final Block block = world.getBlock(i, j, k);
-                final int l = world.getBlockMetadata(i, j, k);
-                if (block == Blocks.obsidian && l == 0) {
-                    world.setBlockToAir(i, j, k);
-                    item.damageItem(1, (EntityLivingBase)player);
-                    return super.onItemRightClick(item, world, player);
-                }
-            }
-
-
+        final MovingObjectPosition movingobjectposition = this.getMovingObjectPositionFromPlayer(par2World, par3EntityPlayer, true);
+        if (movingobjectposition == null) {
+            return par1ItemStack;
         }
-
-
-        return super.onItemRightClick(item, world, player);
+        if (movingobjectposition.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
+            final int i = movingobjectposition.blockX;
+            final int j = movingobjectposition.blockY;
+            final int k = movingobjectposition.blockZ;
+            if (!par2World.canMineBlock(par3EntityPlayer, i, j, k)) {
+                return par1ItemStack;
+            }
+            if (!par3EntityPlayer.canPlayerEdit(i, j, k, movingobjectposition.sideHit, par1ItemStack)) {
+                return par1ItemStack;
+            }
+            final Block block = par2World.getBlock(i, j, k);
+            final int l = par2World.getBlockMetadata(i, j, k);
+            if (block == Blocks.obsidian && l == 0) {
+                par2World.func_147480_a(i, j, k, true);
+                par1ItemStack.damageItem(2, (EntityLivingBase)par3EntityPlayer);
+            }
+        }
+        return par1ItemStack;
     }
 }
